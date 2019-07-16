0254e2cac331cda39c3ecb95cf9130582f70a3ff
Alfie <Alfie2015uk@outlook.com>
2019-07-16 15:35:45 +0100

    added jupyter notebook

--- tweet.py
+++ tweet.py
@@ -1,26 +1,35 @@
 from wordcloud import WordCloud, STOPWORDS
 import tweepy as tw
 import matplotlib.pyplot as plot
+import json
+import re
 
 
+Tweets_dict = dict()
+file = "TweetList.txt"
 Key = 'wjQqcP8hTwOUBHBca7Ens3cZH'
 Secret = 'c6ZNbGfoo4cW0Sy0MgoerWMeaKa9wdH7O1zjljx4MKN6i3onvu'
-
 auth = tw.OAuthHandler(Key, Secret)
 api = tw.API(auth)
-#search_words = '#rdguk OR #Reading2050'
 search_words = ['#rdguk','#Reading2050']
-tweets = tw.Cursor(api.search, q=" OR ".join(search_words), lang='en').items(25)
-TweetsList = [tweet.text for tweet in tweets]
-#print(TweetsList)
-for i in TweetsList:
-    if '#Reading2050' in i:
-        print('test')
-for idx,tweet in enumerate(TweetsList):
-    if 'https' in tweet:
-        TweetsList[idx]= tweet[:tweet.index('https')]
-wc = WordCloud(stopwords = STOPWORDS)
-wc.generate(" ".join(TweetsList))
-plot.imshow(wc)
-plot.show()
-#print(TweetsList)
+terms_grouped = []
+while True:
+    with open(file,"r") as f:
+        Tweets_dict = json.load(f)
+
+    search_terms = ('Traffic','Transport','Mobility','Bicycle','Congestion',
+    'Parking','\"Private Vehicles\"','\"Rapid Transit\"','Pollution','\"Multi Modal\"',
+    'Roads','\"Electric Vehicles\"','Bus','\"Smart Cities\"','Car','Road','Walk',
+    'Pavement','Pedestrian','Electric','\"Traffic lights\"','Motorway')
+    for i in range(0,len(search_terms),4):
+        group = search_terms[i:i+4]
+        query_string = " OR ".join(search_words)+" "+" OR ".join(group)+' -filter:retweets'
+        tweets = tw.Cursor(api.search, q=query_string, lang='en').items(50)
+
+        for tweet in tweets:
+            text = re.sub(r'https:\/\/t.co\S{1,11}', '', tweet.text, flags=re.MULTILINE)
+            Tweets_dict[tweet.id] = text
+            print(text)
+
+    with open(file,"w") as f:
+        f.write(json.dumps(Tweets_dict,ensure_ascii=False))
