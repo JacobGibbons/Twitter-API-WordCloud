f73e0b7fa0cf518f2faf77af4db4cd26ad039b65
Alfie <Alfie2015uk@outlook.com>
2019-07-17 18:05:57 +0100

    added framework for frequency time graphs

--- ReadingWordcloud.py
+++ ReadingWordcloud.py
@@ -3,17 +3,45 @@ import matplotlib.pyplot as plot
 import csv
 
 tweets = list()
+text_time = []
 stopwords = set(STOPWORDS)
 stopwords.update(['RT','co','https','http','rdguk'])
 with open('Reading_TweetList.txt') as f:
     reader = csv.reader(f)
     for id,time,text in reader:
         tweets.append(text)
+        text_time.append([text,time])
 
 
-wc = WordCloud(width = 800, height = 400,stopwords = stopwords)
-wc.generate(" ".join(tweets))
-plot.imshow(wc)
-plot.axis("off")
-plot.rcParams["figure.figsize"] = (20,10)
-plot.show()
+
+def FrequencyTimeGraph(word):
+    #validtweets = []
+    Frequencytable = []
+    Yaxis = []
+    for i in range(24):
+        Frequencytable.append(0)
+        Yaxis.append(i)
+
+    print(len(text_time))
+    x=0
+    for idx,tweet in enumerate(text_time):
+        if word in tweet[0]:
+            x+=1
+            Frequencytable[int(text_time[idx][1].split(" ")[1].split(":")[0])]+=1
+    print(x)
+    print(Frequencytable)
+    plot.plot(Yaxis,Frequencytable,'ro')
+    plot.plot(Yaxis,Frequencytable,'k')
+    plot.ylabel('Frequency')
+    plot.xlabel('Time')
+    plot.title()
+    plot.show()
+FrequencyTimeGraph('road')
+
+
+#wc = WordCloud(width = 800, height = 400,stopwords = stopwords)
+#wc.generate(" ".join(tweets))
+#plot.imshow(wc)
+#plot.axis("off")
+#plot.rcParams["figure.figsize"] = (20,10)
+#plot.show()
