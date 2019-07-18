3797fcaa881c94f7399129122d5c41c491a152f0
Alfie <Alfie2015uk@outlook.com>
2019-07-18 14:07:33 +0100

    updated README

--- ReadingWordcloud.py
+++ ReadingWordcloud.py
@@ -20,23 +20,6 @@ with open('Reading_TweetList.txt') as f:
         elif analysis.sentiment.polarity < 0:
             ntweets.append(text)
 
-
-
-def FrequencyTimeGraph(word):
-    Frequencytable = []
-    Yaxis = []
-    for i in range(24):
-        Frequencytable.append(0)
-        Yaxis.append(i)
-
-    for idx,tweet in enumerate(text_time):
-        if word in tweet[0]:
-            Frequencytable[int(text_time[idx][1].split(" ")[1].split(":")[0])]+=1
-    plot.plot(Yaxis,Frequencytable,'k')
-    plot.plot(Yaxis,Frequencytable,'ro')
-    plot.ylabel('Frequency')
-    plot.xlabel('Time')
-    plot.show()
 positiveproportion = len(ptweets)/len(tweets)
 negativeproportion = len(ntweets)/len(tweets)
 neutralproportion = 1-positiveproportion-negativeproportion
@@ -45,9 +28,9 @@ pielabels = ['Positive','Negative','Neutral']
 plot.pie(sentimentfractions, labels=pielabels, autopct='%.0f%%')
 plot.show()
 
-#wc = WordCloud(width = 800, height = 400,stopwords = stopwords)
-#wc.generate(" ".join(tweets))
-#plot.imshow(wc)
-#plot.axis("off")
-#plot.rcParams["figure.figsize"] = (20,10)
-#plot.show()
+wc = WordCloud(width = 800, height = 400,stopwords = stopwords)
+wc.generate(" ".join(tweets))
+plot.figure(figsize=(20,10))
+plot.imshow(wc)
+plot.axis("off")
+plot.show()
