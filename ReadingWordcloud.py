4fdd18d16f7bd1cc35b43fc2fb780606ea0ef93a
Benjamin Hodges <benjaminhodges07@gmail.com>
2019-07-18 12:43:00 +0100

    added sentiment

--- ReadingWordcloud.py
+++ ReadingWordcloud.py
@@ -1,7 +1,10 @@
 from wordcloud import WordCloud, STOPWORDS
 import matplotlib.pyplot as plot
 import csv
+from textblob import TextBlob
 
+ptweets = list()
+ntweets = list()
 tweets = list()
 text_time = []
 stopwords = set(STOPWORDS)
@@ -11,33 +14,36 @@ with open('Reading_TweetList.txt') as f:
     for id,time,text in reader:
         tweets.append(text)
         text_time.append([text,time])
+        analysis = TextBlob(text)
+        if analysis.sentiment.polarity > 0:
+            ptweets.append(text)
+        elif analysis.sentiment.polarity < 0:
+            ntweets.append(text)
 
 
 
 def FrequencyTimeGraph(word):
-    #validtweets = []
     Frequencytable = []
     Yaxis = []
     for i in range(24):
         Frequencytable.append(0)
         Yaxis.append(i)
 
-    print(len(text_time))
-    x=0
     for idx,tweet in enumerate(text_time):
         if word in tweet[0]:
-            x+=1
             Frequencytable[int(text_time[idx][1].split(" ")[1].split(":")[0])]+=1
-    print(x)
-    print(Frequencytable)
-    plot.plot(Yaxis,Frequencytable,'ro')
     plot.plot(Yaxis,Frequencytable,'k')
+    plot.plot(Yaxis,Frequencytable,'ro')
     plot.ylabel('Frequency')
     plot.xlabel('Time')
-    plot.title()
     plot.show()
-FrequencyTimeGraph('road')
-
+positiveproportion = len(ptweets)/len(tweets)
+negativeproportion = len(ntweets)/len(tweets)
+neutralproportion = 1-positiveproportion-negativeproportion
+sentimentfractions = [positiveproportion,negativeproportion,neutralproportion]
+pielabels = ['Positive','Negative','Neutral']
+plot.pie(sentimentfractions, labels=pielabels, autopct='%.0f%%')
+plot.show()
 
 #wc = WordCloud(width = 800, height = 400,stopwords = stopwords)
 #wc.generate(" ".join(tweets))
