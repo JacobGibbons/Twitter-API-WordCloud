0e3ff00421095953c4463c6e8481571ac47b7b28
Alfie <Alfie2015uk@outlook.com>
2019-07-19 13:14:10 +0100

    removed redundant files

--- VadAndBarWordcloud.py
+++
@@ -1,9 +0,0 @@
-from sentiment import Sentiment
-
-
-s = Sentiment('VandB_TweetList.txt')
-
-wordlist = (['RT','co','https','http','Baroda','Vadodara','SmartCitiesIndia'])
-s.show_piechart()
-s.show_worldcloud(wordlist)
-s.FrequencyTimeGraph('Mall')
